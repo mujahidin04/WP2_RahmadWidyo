ce4be569d0a37996085655195787441c60d936d6
mujahidin04 <celake001@gmail.com>
2022-09-18 21:27:54 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h1>Perkenalkkan</h1>";
+        echo "saya rahmad
+        dari daerah kubu raya
+        olahraga yang saya sukaii adalah
+        sepak bola";
+	}
+}
